12106461722387b4722089f3f1bc1d429f940dcb
Pranoy R <pranoyalkr@gmail.com>
2019-12-04 20:19:28 +0530

    updated

--- model.py
+++ model.py
@@ -7,10 +7,8 @@ def generate_model(opt):
     # assert opt.model in [
     #     'resnet', 'preresnet', 'wideresnet', 'resnext', 'densenet'
     # ]
-
     if opt.model == 'resnet':
-        model = resnet.Net(num_classes=70)
-        
+        model = resnet.Net(num_classes=70)  
     elif opt.model == 'vgg':
         model = vgg.Net(num_classes=70) 
         
