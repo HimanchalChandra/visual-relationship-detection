48a5e596cf1d7fcb3b1d1c1f38eb17acf9b8e118
Pranoy R <pranoyalkr@gmail.com>
2020-01-15 23:11:30 +0530

    added only mfurln

--- model.py
+++ model.py
@@ -1,18 +1,19 @@
 import torch
 from torch import nn
-from models import resnet, vgg, vs_sw, mfurln
+# from models import resnet, vgg, vs_sw, mfurln
+from models import mfurln
 
 
 def generate_model(opt):
     # assert opt.model in [
     #     'resnet', 'preresnet', 'wideresnet', 'resnext', 'densenet'
     # ]
-    if opt.model == 'resnet':
-        model = resnet.Net(opt.num_classes)  
-    elif opt.model == 'vgg':
-        model = vgg.Net(opt.num_classes) 
-    elif opt.model == 'vs_sw':
-        model = vs_sw.Net(opt.num_classes) 
+    # if opt.model == 'resnet':
+    #     model = resnet.Net(opt.num_classes)  
+    # elif opt.model == 'vgg':
+    #     model = vgg.Net(opt.num_classes) 
+    # elif opt.model == 'vs_sw':
+    #     model = vs_sw.Net(opt.num_classes) 
     elif opt.model == 'mfurln':
         model = mfurln.Net(opt.num_classes) 
 
