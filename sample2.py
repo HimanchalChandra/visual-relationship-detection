6e9ce4286f2469055e803b4d6e2892a5d608ef42
Pranoy R <pranoyalkr@gmail.com>
2020-01-17 13:23:11 +0530

    updated

--- sample2.py
+++ sample2.py
@@ -3,25 +3,15 @@ import torch
 import pandas as pd
 from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-import torch.optim as optim
-from torchvision.models import vgg16
-from PIL import Image, ImageFont, ImageDraw
-from dataset import VrdDataset
+
 import json
 import torch.nn as nn
-
+import torch
 import argparse
 import random
 import cv2
 from models import MFURLN
-from shapely.geometry import box
-from shapely.ops import cascaded_union
-from retinanet.obj_det_retinanet import ObjDetRetinanet
-from utils.util import calc_iou,calc_intersection
-from opts import parse_opts
+
 # model = vgg16(pretrained=True)
 # modules = list(model.children())[:-2]  
 # base_net = nn.Sequential(*modules)
