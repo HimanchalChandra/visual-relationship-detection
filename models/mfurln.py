4137bbe7eb6d1e7d1dfbc3f24ee0d00a4075d50a
Pranoy R <pranoyalkr@gmail.com>
2020-01-17 23:38:41 +0530

    addded roi pooling

--- models/mfurln.py
+++ models/mfurln.py
@@ -103,8 +103,12 @@ class VisionModule(nn.Module):
 		x = self.vgg_backbone(x)
 		x_sub = self.roi_pool(x, rois[0])
 		x_obj = self.roi_pool(x, rois[1])
-		x = torch.cat([x, x_sub, x_obj], dim=1)
+
 		x = x.view(x.size(0), -1)
+		x_sub = x_sub.view(x_sub.size(0), -1)
+		x_obj = x_obj.view(x_obj.size(0), -1)
+		
+		x = torch.cat([x, x_sub, x_obj], dim=1)
 		print(x.size())
 		x = self.fc(x)
 		x = F.relu(x)
