fb7ab04c4714a1ed3937a31add35f326cef64af5
Pranoy R <pranoyalkr@gmail.com>
2020-01-18 00:31:05 +0530

    addded roi pooling

--- models/mfurln.py
+++ models/mfurln.py
@@ -100,7 +100,7 @@ class VisionModule(nn.Module):
 		self.fc = nn.Linear(512, 4096)
 
 	def forward(self, x, rois):
-    	print(x.size())
+		print(x.size())
 		x = self.vgg_backbone(x)
 		x_sub = self.roi_pool(x, rois[0])
 		x_obj = self.roi_pool(x, rois[1])
