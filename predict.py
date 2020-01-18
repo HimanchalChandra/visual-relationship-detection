e4b009edd9fc1f3734e52495088072e129e3eb83
Pranoy R <pranoyalkr@gmail.com>
2020-01-18 21:11:47 +0530

    updated

--- predict.py
+++ predict.py
@@ -152,7 +152,8 @@ def main():
 			print(spatial_locations.shape)
 			print(word_vectors.shape)
 
-			confidences, predicates = model(imgs, spatial_locations, word_vectors)
+			with torch.no_grad():
+				confidences, predicates = model(imgs, spatial_locations, word_vectors)
 			confidences = torch.sigmoid(confidences)
 			predicates = torch.sigmoid(predicates)
 
