282e6f1aab4e5d5a5db7ce45818f2eeb9704d104
Pranoy R <pranoyalkr@gmail.com>
2020-01-22 23:30:35 +0530

    upadted

--- datasets/vrd.py
+++ datasets/vrd.py
@@ -116,156 +116,156 @@ class VrdDataset(Dataset):
 		rois_obj = []
 		
 
-		detection1 = detection.copy()
-		for sub in detection:
-			for obj in detection1:
-				sub_label = self.class2idx_obj[sub['label']]
-				obj_label = self.class2idx_obj[obj['label']]
-				sub_bbox = [int(sub['bbox'][0]), int(sub['bbox'][1]), int(sub['bbox'][2]), int(sub['bbox'][3])]
-				obj_bbox = [int(obj['bbox'][0]), int(obj['bbox'][1]), int(obj['bbox'][2]), int(obj['bbox'][3])]
+		# detection1 = detection.copy()
+		# for sub in detection:
+		# 	for obj in detection1:
+		# 		sub_label = self.class2idx_obj[sub['label']]
+		# 		obj_label = self.class2idx_obj[obj['label']]
+		# 		sub_bbox = [int(sub['bbox'][0]), int(sub['bbox'][1]), int(sub['bbox'][2]), int(sub['bbox'][3])]
+		# 		obj_bbox = [int(obj['bbox'][0]), int(obj['bbox'][1]), int(obj['bbox'][2]), int(obj['bbox'][3])]
 
-				if sub_label == obj_label and sub_bbox == obj_bbox:
-					continue
-				# takes union of sub and obj
-				polygons = [box(sub_bbox[0], sub_bbox[1], sub_bbox[2], sub_bbox[3]),
-								box(obj_bbox[0], obj_bbox[1], obj_bbox[2], obj_bbox[3])]
-				unioned = cascaded_union(polygons)
-				unioned = unioned.bounds
-				xmin_unioned, ymin_unioned, xmax_unioned, ymax_unioned = unioned
-				# crop image
-				cropped_img = img.crop((int(xmin_unioned), int(
-					ymin_unioned), int(xmax_unioned), int(ymax_unioned)))
+		# 		if sub_label == obj_label and sub_bbox == obj_bbox:
+		# 			continue
+		# 		# takes union of sub and obj
+		# 		polygons = [box(sub_bbox[0], sub_bbox[1], sub_bbox[2], sub_bbox[3]),
+		# 						box(obj_bbox[0], obj_bbox[1], obj_bbox[2], obj_bbox[3])]
+		# 		unioned = cascaded_union(polygons)
+		# 		unioned = unioned.bounds
+		# 		xmin_unioned, ymin_unioned, xmax_unioned, ymax_unioned = unioned
+		# 		# crop image
+		# 		cropped_img = img.crop((int(xmin_unioned), int(
+		# 			ymin_unioned), int(xmax_unioned), int(ymax_unioned)))
 
-				img_w, img_h = cropped_img.size
-				factor_h = img_h/224
-				factor_w = img_w/224
+		# 		img_w, img_h = cropped_img.size
+		# 		factor_h = img_h/224
+		# 		factor_w = img_w/224
 
-				cropped_img = self.transform(cropped_img)
-				cropped_imgs.append(cropped_img)
+		# 		cropped_img = self.transform(cropped_img)
+		# 		cropped_imgs.append(cropped_img)
 
-				# prepare  spatial locations
-				sub_xmin = sub_bbox[0]
-				sub_ymin = sub_bbox[1]
-				sub_xmax = sub_bbox[2]
-				sub_ymax = sub_bbox[3]
-				obj_xmin = obj_bbox[0]
-				obj_ymin = obj_bbox[1]
-				obj_xmax = obj_bbox[2]
-				obj_ymax = obj_bbox[3]
+		# 		# prepare  spatial locations
+		# 		sub_xmin = sub_bbox[0]
+		# 		sub_ymin = sub_bbox[1]
+		# 		sub_xmax = sub_bbox[2]
+		# 		sub_ymax = sub_bbox[3]
+		# 		obj_xmin = obj_bbox[0]
+		# 		obj_ymin = obj_bbox[1]
+		# 		obj_xmax = obj_bbox[2]
+		# 		obj_ymax = obj_bbox[3]
 
-				sub_x1 = int((sub_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
-				sub_y1 = int((sub_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
-				sub_x2 = int((sub_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
-				sub_y2 = int((sub_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
+		# 		sub_x1 = int((sub_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
+		# 		sub_y1 = int((sub_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
+		# 		sub_x2 = int((sub_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
+		# 		sub_y2 = int((sub_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
 
-				obj_x1 = int((obj_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
-				obj_y1 = int((obj_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
-				obj_x2 = int((obj_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
-				obj_y2 = int((obj_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
+		# 		obj_x1 = int((obj_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
+		# 		obj_y1 = int((obj_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
+		# 		obj_x2 = int((obj_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
+		# 		obj_y2 = int((obj_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
 
-				spatial_locations.append([sub_x1, sub_y1, sub_x2, sub_y2, obj_x1, obj_y1, obj_x2, obj_y2])
+		# 		spatial_locations.append([sub_x1, sub_y1, sub_x2, sub_y2, obj_x1, obj_y1, obj_x2, obj_y2])
 
-				# # prepare rois
-				rois = self.prepare_rois(sub_bbox, obj_bbox, unioned, factor_h, factor_w)
-				rois_sub.append(rois['sub'])
-				rois_obj.append(rois['obj'])
+		# 		# # prepare rois
+		# 		rois = self.prepare_rois(sub_bbox, obj_bbox, unioned, factor_h, factor_w)
+		# 		rois_sub.append(rois['sub'])
+		# 		rois_obj.append(rois['obj'])
 
-				# prepare word vectors
-				word_vectors.append([sub_label, obj_label])
+		# 		# prepare word vectors
+		# 		word_vectors.append([sub_label, obj_label])
 
-				data_type = 'undetermined'
-				for sub_pred_obj in annotation:
-					sub_label_gt = sub_pred_obj['subject']['category']
-					obj_label_gt = sub_pred_obj['object']['category']
-					sub_bbox_gt = sub_pred_obj['subject']['bbox']
-					obj_bbox_gt = sub_pred_obj['object']['bbox']
-					# convert to x1, y1, x2, y2
-					sub_bbox_gt = [sub_bbox_gt[2], sub_bbox_gt[0], sub_bbox_gt[3], sub_bbox_gt[1]]
-					obj_bbox_gt = [obj_bbox_gt[2], obj_bbox_gt[0], obj_bbox_gt[3], obj_bbox_gt[1]]
+		# 		data_type = 'undetermined'
+		# 		for sub_pred_obj in annotation:
+		# 			sub_label_gt = sub_pred_obj['subject']['category']
+		# 			obj_label_gt = sub_pred_obj['object']['category']
+		# 			sub_bbox_gt = sub_pred_obj['subject']['bbox']
+		# 			obj_bbox_gt = sub_pred_obj['object']['bbox']
+		# 			# convert to x1, y1, x2, y2
+		# 			sub_bbox_gt = [sub_bbox_gt[2], sub_bbox_gt[0], sub_bbox_gt[3], sub_bbox_gt[1]]
+		# 			obj_bbox_gt = [obj_bbox_gt[2], obj_bbox_gt[0], obj_bbox_gt[3], obj_bbox_gt[1]]
 
-					# calculates iou of detections and gt for sub and obj.
-					sub_iou = calc_iou(sub_bbox, sub_bbox_gt)
-					obj_ioc = calc_iou(obj_bbox, obj_bbox_gt)
+		# 			# calculates iou of detections and gt for sub and obj.
+		# 			sub_iou = calc_iou(sub_bbox, sub_bbox_gt)
+		# 			obj_ioc = calc_iou(obj_bbox, obj_bbox_gt)
 
-					# prepare predicates for determined and undetermined data
-					if (sub_label == sub_label_gt and obj_label == obj_label_gt and sub_iou > 0.5 and obj_ioc > 0.5):
-						data_type = 'determined'
-						break
+		# 			# prepare predicates for determined and undetermined data
+		# 			if (sub_label == sub_label_gt and obj_label == obj_label_gt and sub_iou > 0.5 and obj_ioc > 0.5):
+		# 				data_type = 'determined'
+		# 				break
 				
-				if (data_type == 'determined'):
-					predicate = sub_pred_obj['predicate']
-					#predicate = one_hot_encode(predicate, self.num_classes)
-					predicate_list.append(predicate)
-					#binary_targets.append(one_hot_encode(1, num_classes = 2))
-					binary_targets.append(1)
-				else:
-					# predicate = [0 for _ in range(self.num_classes)]
-					#predicate = one_hot_encode(70, self.num_classes)
-					predicate_list.append(70)
-					binary_targets.append(0)
+		# 		if (data_type == 'determined'):
+		# 			predicate = sub_pred_obj['predicate']
+		# 			#predicate = one_hot_encode(predicate, self.num_classes)
+		# 			predicate_list.append(predicate)
+		# 			#binary_targets.append(one_hot_encode(1, num_classes = 2))
+		# 			binary_targets.append(1)
+		# 		else:
+		# 			# predicate = [0 for _ in range(self.num_classes)]
+		# 			#predicate = one_hot_encode(70, self.num_classes)
+		# 			predicate_list.append(70)
+		# 			binary_targets.append(0)
 
-		if not cropped_imgs:
-			for sub_pred_obj in annotation:
-				sub_label_gt = sub_pred_obj['subject']['category']
-				obj_label_gt = sub_pred_obj['object']['category']
-				sub_bbox_gt = sub_pred_obj['subject']['bbox']
-				obj_bbox_gt = sub_pred_obj['object']['bbox']
-				# convert to x1, y1, x2, y2
-				sub_bbox_gt = [sub_bbox_gt[2], sub_bbox_gt[0], sub_bbox_gt[3], sub_bbox_gt[1]]
-				obj_bbox_gt = [obj_bbox_gt[2], obj_bbox_gt[0], obj_bbox_gt[3], obj_bbox_gt[1]]
+		# if not cropped_imgs:
+		for sub_pred_obj in annotation:
+			sub_label_gt = sub_pred_obj['subject']['category']
+			obj_label_gt = sub_pred_obj['object']['category']
+			sub_bbox_gt = sub_pred_obj['subject']['bbox']
+			obj_bbox_gt = sub_pred_obj['object']['bbox']
+			# convert to x1, y1, x2, y2
+			sub_bbox_gt = [sub_bbox_gt[2], sub_bbox_gt[0], sub_bbox_gt[3], sub_bbox_gt[1]]
+			obj_bbox_gt = [obj_bbox_gt[2], obj_bbox_gt[0], obj_bbox_gt[3], obj_bbox_gt[1]]
 
-				# takes union of sub and obj
-				polygons = [box(sub_bbox_gt[0], sub_bbox_gt[1], sub_bbox_gt[2], sub_bbox_gt[3]),
-								box(obj_bbox_gt[0], obj_bbox_gt[1], obj_bbox_gt[2], obj_bbox_gt[3])]
-				unioned = cascaded_union(polygons)
-				unioned = unioned.bounds
-				xmin_unioned, ymin_unioned, xmax_unioned, ymax_unioned = unioned
-				# crop image
-				cropped_img = img.crop((int(xmin_unioned), int(
-					ymin_unioned), int(xmax_unioned), int(ymax_unioned)))
+			# takes union of sub and obj
+			polygons = [box(sub_bbox_gt[0], sub_bbox_gt[1], sub_bbox_gt[2], sub_bbox_gt[3]),
+							box(obj_bbox_gt[0], obj_bbox_gt[1], obj_bbox_gt[2], obj_bbox_gt[3])]
+			unioned = cascaded_union(polygons)
+			unioned = unioned.bounds
+			xmin_unioned, ymin_unioned, xmax_unioned, ymax_unioned = unioned
+			# crop image
+			cropped_img = img.crop((int(xmin_unioned), int(
+				ymin_unioned), int(xmax_unioned), int(ymax_unioned)))
 
-				img_w, img_h = cropped_img.size
-				factor_h = img_h/224
-				factor_w = img_w/224
+			img_w, img_h = cropped_img.size
+			factor_h = img_h/224
+			factor_w = img_w/224
 
-				cropped_img = self.transform(cropped_img)
-				cropped_imgs.append(cropped_img)
+			cropped_img = self.transform(cropped_img)
+			cropped_imgs.append(cropped_img)
 
-				# prepare  spatial locations
-				sub_xmin = sub_bbox_gt[0]
-				sub_ymin = sub_bbox_gt[1]
-				sub_xmax = sub_bbox_gt[2]
-				sub_ymax = sub_bbox_gt[3]
-				obj_xmin = obj_bbox_gt[0]
-				obj_ymin = obj_bbox_gt[1]
-				obj_xmax = obj_bbox_gt[2]
-				obj_ymax = obj_bbox_gt[3]
+			# prepare  spatial locations
+			sub_xmin = sub_bbox_gt[0]
+			sub_ymin = sub_bbox_gt[1]
+			sub_xmax = sub_bbox_gt[2]
+			sub_ymax = sub_bbox_gt[3]
+			obj_xmin = obj_bbox_gt[0]
+			obj_ymin = obj_bbox_gt[1]
+			obj_xmax = obj_bbox_gt[2]
+			obj_ymax = obj_bbox_gt[3]
 
-				sub_x1 = int((sub_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
-				sub_y1 = int((sub_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
-				sub_x2 = int((sub_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
-				sub_y2 = int((sub_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
+			sub_x1 = int((sub_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
+			sub_y1 = int((sub_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
+			sub_x2 = int((sub_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
+			sub_y2 = int((sub_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
 
-				obj_x1 = int((obj_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
-				obj_y1 = int((obj_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
-				obj_x2 = int((obj_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
-				obj_y2 = int((obj_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
+			obj_x1 = int((obj_xmin - xmin_unioned)/(xmax_unioned - xmin_unioned))
+			obj_y1 = int((obj_ymin - ymin_unioned)/(ymax_unioned - ymin_unioned))
+			obj_x2 = int((obj_xmax - xmax_unioned)/(xmax_unioned - xmin_unioned))
+			obj_y2 = int((obj_ymax - ymax_unioned)/(ymax_unioned - ymin_unioned))
 
-				spatial_locations.append([sub_x1, sub_y1, sub_x2, sub_y2, obj_x1, obj_y1, obj_x2, obj_y2])
+			spatial_locations.append([sub_x1, sub_y1, sub_x2, sub_y2, obj_x1, obj_y1, obj_x2, obj_y2])
 
-				# prepare rois
-				rois = self.prepare_rois(sub_bbox_gt, obj_bbox_gt, unioned, factor_h, factor_w)
-				rois_sub.append(rois['sub'])
-				rois_obj.append(rois['obj'])
+			# prepare rois
+			rois = self.prepare_rois(sub_bbox_gt, obj_bbox_gt, unioned, factor_h, factor_w)
+			rois_sub.append(rois['sub'])
+			rois_obj.append(rois['obj'])
 
-				# prepare word vectors
-				word_vectors.append([sub_label_gt, obj_label_gt])
-				
-				# prepare predicates
-				predicate = sub_pred_obj['predicate']
-				#predicate = one_hot_encode(predicate, self.num_classes)
-				predicate_list.append(predicate)
-				binary_targets.append(1)
+			# prepare word vectors
+			word_vectors.append([sub_label_gt, obj_label_gt])
+			
+			# prepare predicates
+			predicate = sub_pred_obj['predicate']
+			#predicate = one_hot_encode(predicate, self.num_classes)
+			predicate_list.append(predicate)
+			binary_targets.append(1)
 			
 
 		imgs = torch.stack(cropped_imgs)
