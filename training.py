1813972252811848750c435524334a4a3ebbcf57
Pranoy R <pranoyalkr@gmail.com>
2020-01-22 23:24:16 +0530

    upadted

--- training.py
+++ training.py
@@ -51,9 +51,9 @@ def train(model, loader, criterion, optimizer, epoch, device, opt):
         tot_loss = loss1 + loss2
         train_loss += tot_loss.item()
 
-        # losses.update(tot_loss.item(), imgs.size(0))
-        # predicates = F.softmax(predicates, dim=1)
-        # metric.update(predicates, targets_predicates)
+        losses.update(tot_loss.item(), imgs.size(0))
+        predicates = F.softmax(predicates, dim=1)
+        metric.update(predicates, targets_predicates)
 
         optimizer.zero_grad()
         tot_loss.backward()
