f6e8bd8576fb1258666dc9327285ff6e5ebad6fb
Pranoy R <pranoyalkr@gmail.com>
2020-02-01 19:28:03 +0530

    updated

--- validation.py
+++ validation.py
@@ -21,7 +21,6 @@ def validate(model, loader, criterion, epoch, device, opt):
     model.eval()
 
     losses = AverageMeter()
-    metric = AverageMeter()
     metric = Metric(opt.num_classes)
     with torch.no_grad():
         for i, (imgs, spatial_locations, word_vectors, targets_predicates, targets_confidences) in enumerate(loader):
