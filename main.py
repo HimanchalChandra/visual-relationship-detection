0da2634a429ecb4b3cabc4ac2eb8df22b153c4c7
Pranoy R <pranoyalkr@gmail.com>
2020-02-02 18:25:54 +0530

    dfsd

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def main():
 	model = model.cuda()
 
 	if torch.cuda.device_count() > 1:
-    	  	print("Let's use", torch.cuda.device_count(), "GPUs!")
+		  	print("Let's use", torch.cuda.device_count(), "GPUs!")
   			model = nn.DataParallel(model)
 
 	if opt.nesterov:
